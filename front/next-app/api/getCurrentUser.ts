87a28838c9f0327d35d8ff6fdd9f6632e5b66a77
junpei-chan <jumpei24s1102270@gmail.com>
2025-11-17 04:26:24 +0900

    add: getCurrentUesr.ts

---
+++ front/next-app/api/getCurrentUser.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export interface GetCurrentUserResponse {
+  name: string;
+  iconSrc: string;
+}
+
+export async function GetCurrentUser():Promise<GetCurrentUserResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/user`;
+  const authToken = Cookies.get("authToken");
+
+  return axios
+    .get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        Accept: "application/json"
+      }
+    })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
