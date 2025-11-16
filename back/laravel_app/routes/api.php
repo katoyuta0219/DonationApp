bc2fd8990a51c0dd4b21def8385864cfbd7ec798
katoyuta <katoyuta021962@gmail.com>
2025-11-16 11:58:09 +0900

    sign_upAPIいったん完成

--- back/laravel_app/routes/api.php
+++ back/laravel_app/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\SignUpController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/api/auth/signup', [SignUpController::class, 'sign_up']);
