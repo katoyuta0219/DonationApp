03357698d8f6d8969857deb605956db26168d9d3
junpei-chan <jumpei24s1102270@gmail.com>
2025-11-17 03:25:47 +0900

    create: Shipping Page

--- front/next-app/app/shipping/page.tsx
+++ front/next-app/app/shipping/page.tsx
@@ -38,7 +38,7 @@ export default function Shipping() {
         以下の手順に従って寄付品をお送りください。団体ごとに指定がある場合は、詳細ページも必ずご確認ください。
       </p>
       <div>
-        <div className="w-53 flex justify-between items-end mx-auto">
+        <div className="w-53 flex justify-between items-end mx-auto mb-10">
           {[...Array(3)].map((_, i) => {
             const isActive = currentStep === i+1;
             const isCompleted = currentStep > i+1;
@@ -88,11 +88,13 @@ export default function Shipping() {
         <div>
           {currentStep === 1 ? (
             <>
-              <section>
-                <h2>1. 発送方法</h2>
-                <p>寄付する荷物は、全国のコンビニから発送できます。</p>
-                <p> 梱包した荷物をお近くのコンビニにお持ち込みください。</p>
-                <p> このあと、発送に必要なQRコードと手順をご案内します。</p>
+              <section className="flex flex-col gap-6 w-82 mx-auto text-black">
+                <h2 className="Body16Medium">1. 発送方法</h2>
+                <div className="Body12Regular">
+                  <p>寄付する荷物は、全国のコンビニから発送できます。</p>
+                  <p> 梱包した荷物をお近くのコンビニにお持ち込みください。</p>
+                  <p> このあと、発送に必要なQRコードと手順をご案内します。</p>
+                </div>
               </section>
               <Button 
                 size="large"
@@ -100,29 +102,36 @@ export default function Shipping() {
                 iconSrc="/icons/right-arrow/white.svg"
                 iconAlt="right-arrow-icon.svg"
                 iconPosition="right"
+                className="absolute bottom-10 left-1/2 -translate-x-1/2"
                 onClick={() => handleNextStep(2)}
               />
             </>
           ): currentStep === 2 ? (
             <>
-              <section>
-                <h2>2. 送り状の発行</h2>
-                <p>このQRコードを（コンビニなどの）店員にご提示ください。</p>
-                <p>店員がお手続きを進め、送り状が発行されます。</p>
+              <section className="flex flex-col gap-6 w-82 mx-auto text-black">
+                <h2 className="Body16Medium">2. 送り状の発行</h2>
+                <div className="Body12Regular">
+                  <p>このQRコードを（コンビニなどの）店員にご提示ください。</p>
+                  <p>店員がお手続きを進め、送り状が発行されます。</p>
+                </div>
               </section>
-              <button onClick={() => handleNextStep(3)}>
+              <button 
+                onClick={() => handleNextStep(3)}
+                className="absolute bottom-10 left-1/2 -translate-x-1/2"
+              >
                 <QRCodeSVG value="https://github.com/katoyuta0219/DonationApp" />
               </button>
             </>
           ): currentStep === 3 && (
             <>
-              <section>
-                <h2>3. 発送完了</h2>
-                <p>発送が完了しました！</p>
+              <section className="flex flex-col gap-6 w-82 mx-auto text-black">
+                <h2 className="Body16Medium">3. 発送完了</h2>
+                <p className="Body12Regular">発送が完了しました！</p>
               </section>
               <Button 
                 size="large"
                 text="ホームに戻る"
+                className="absolute bottom-10 left-1/2 -translate-x-1/2"
                 onClick={() => handleBackHome()}
               />
             </>
