b39aac4970d19cfd38b8341c606fd3d8bf317bf3
katoyuta <katoyuta021962@gmail.com>
2025-11-17 11:54:14 +0900

    API微調整（足りない箇所の追加）

--- back/laravel_app/app/Http/Controllers/OrganizationController.php
+++ back/laravel_app/app/Http/Controllers/OrganizationController.php
@@ -8,16 +8,23 @@ use Illuminate\Validation\ValidationException;
 
 class OrganizationController extends Controller
 {
-    public function show($id){
+    public function show($id)
+    {
         try {
-        $show = Organization::find($id);
-        return response($show);
-        } catch (\Exception $e){
+            // $show = Organization::with(['donationRequests:organization_id,degree_of_necessity,deadline',
+            //                             'donationRequest.requestCategories:id,name'
+            // ])->find($id);
+            $show = Organization::with([
+                'donationRequests',
+                'donationRequests.requestCategories'
+            ])->find($id);
+            return response($show);
+        } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
                 'message' => '取得に失敗しました'
             ]);
-        } catch (ValidationException $e){
+        } catch (ValidationException $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'バリデーションエラー',
