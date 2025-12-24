ea48f142cdfbb6a17342ff77d7844cdd56f9b9eb
junpei-chan <jumpei24s1102270@gmail.com>
2025-12-24 15:17:14 +0900

    change: shipping page

---
+++ front/next-app/app/shipping/method/page.tsx
@@ -0,0 +1,7 @@
+export default function Method() {
+  return (
+    <main className="flex items-center justify-center w-screen h-screen">
+      誰かWebライターを連れてきてください
+    </main>
+  )
+}
--- front/next-app/app/shipping/page.tsx
+++ front/next-app/app/shipping/page.tsx
@@ -1,22 +1,29 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { QRCodeSVG } from "qrcode.react";
 import { Button } from "@/components/shared";
 
 export default function Shipping() {
   const router = useRouter();
-  const [currentStep, setCurrentStep] = useState(1);
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
-  const handleNextStep = (step: number) => {
-    setCurrentStep(step);
+  const shippingOptions = [
+    { key: "convenience", text: "コンビニから送る" },
+    { key: "box", text: "寄付ボックスから送る" },
+  ];
+
+  const handleMethodToggle = (index: number) => {
+    setSelectedIndex(selectedIndex === index ? null : index);
   }
 
-  const handleBackHome = () => {
-    router.push("/");
-  };
+  const handleRouterPush = () => {
+    if (selectedIndex !== null) {
+      router.push(`/shipping/${shippingOptions[selectedIndex].key}`);
+    }
+  }
 
   return (
     <main>
@@ -34,109 +41,69 @@ export default function Shipping() {
         </button>
         <h1 className="Body16Medium text-black">発送の手順</h1>
       </div>
-      <p className="w-78 mt-6 mb-10.5 mx-auto text-black Body14Regular">
-        以下の手順に従って寄付品をお送りください。団体ごとに指定がある場合は、詳細ページも必ずご確認ください。
-      </p>
-      <div>
-        <div className="w-53 flex justify-between items-end mx-auto mb-10">
-          {[...Array(3)].map((_, i) => {
-            const isActive = currentStep === i+1;
-            const isCompleted = currentStep > i+1;
+      <div className="flex flex-col gap-10 pt-8 w-82 mx-auto">
+        <section className="text-black">
+          <h2 className="Body16Medium">
+            発送場所
+          </h2>
+          <div className="Body12Regular mt-5">
+            <p>下記の選択肢から発送場所を選択してください。</p>
+            <p>寄付する荷物は全国のコンビニ、または指定の寄付ボックスから発送する事ができます。</p>
+            <Link 
+              href="/shipping/method"
+              className="flex items-center gap-1.5 mt-3 text-rose-pink-500"
+            >
+              <Image
+                src="/icons/question/rose-pink-500.svg"
+                alt="question-icon"
+                width={16}
+                height={16}
+              />
+              <p>発送方法について</p>
+            </Link>
+          </div>
+        </section>
+        <div className="flex gap-6 Body12Medium">
+          {shippingOptions.map((btn, i) => {
+            const isChecked = selectedIndex === i;
 
             return (
-              <div 
+              <button
                 key={i}
-                className="relative"
+                onClick={() => handleMethodToggle(i)}
+                className={`
+                  relative flex flex-col items-center justify-center gap-2 w-38 h-38 rounded-lg
+                  ${isChecked
+                    ? "bg-rose-pink-100 border-3 border-rose-pink-800 text-rose-pink-700" 
+                    : "bg-white border border-gray-300 text-black"
+                  }
+                `}
               >
-                <>
-                  {isActive && (
-                    <Image
-                      src="/icons/less-than/gray-700.svg"
-                      alt="less-than-icon"
-                      width={24}
-                      height={12}
-                      className="rotate-0"
-                    />
-                  )}
-                  <span
-                    className={`
-                      ${isActive
-                        ? "border-beige-orange-600 text-white bg-beige-orange-600"
-                        : isCompleted
-                        ? "border-beige-orange-600 bg-beige-orange-600 bg-[url(/icons/check/white.svg)] bg-center"
-                        : "border-gray-700 text-gray-700 bg-white"
-                      }
-                      flex items-center justify-center w-6 h-6 mt-1.5 Body12Regular rounded-full border-2
-                    `}
-                  >
-                    {!isCompleted && i+1}
-                  </span>
-                </>
-                {i < 2 && (
-                  <div className={`
-                    ${currentStep > i + 1
-                      ? "bg-beige-orange-600"
-                      : "bg-gray-700"
-                    }
-                    absolute left-6 bottom-3 block w-18 h-0.5
-                  `} />
-                )}
-              </div>
+                <input
+                  type="checkbox"
+                  className={`
+                    appearance-none absolute w-4 h-4 top-2.5 left-2.5 bg-white rounded-full
+                    ${isChecked ? "border-5 border-rose-pink-800" : "border-[1.5px] border-gray-300"}
+                  `}
+                />
+                <Image
+                  src={`/icons/${btn.key}/${isChecked ? "rose-pink-700" : "black"}.svg`}
+                  alt={`${btn.key}-icon`}
+                  width={40}
+                  height={40}
+                />
+                <p>{btn.text}</p>
+              </button>
             );
           })}
         </div>
-        <div>
-          {currentStep === 1 ? (
-            <>
-              <section className="flex flex-col gap-6 w-82 mx-auto text-black">
-                <h2 className="Body16Medium">1. 発送方法</h2>
-                <div className="Body12Regular">
-                  <p>寄付する荷物は、全国のコンビニから発送できます。</p>
-                  <p> 梱包した荷物をお近くのコンビニにお持ち込みください。</p>
-                  <p> このあと、発送に必要なQRコードと手順をご案内します。</p>
-                </div>
-              </section>
-              <Button 
-                size="large"
-                text="次へ"
-                iconSrc="/icons/right-arrow/white.svg"
-                iconAlt="right-arrow-icon.svg"
-                iconPosition="right"
-                className="absolute bottom-10 left-1/2 -translate-x-1/2"
-                onClick={() => handleNextStep(2)}
-              />
-            </>
-          ): currentStep === 2 ? (
-            <>
-              <section className="flex flex-col gap-6 w-82 mx-auto text-black">
-                <h2 className="Body16Medium">2. 送り状の発行</h2>
-                <div className="Body12Regular">
-                  <p>このQRコードを（コンビニなどの）店員にご提示ください。</p>
-                  <p>店員がお手続きを進め、送り状が発行されます。</p>
-                </div>
-              </section>
-              <button 
-                onClick={() => handleNextStep(3)}
-                className="absolute bottom-10 left-1/2 -translate-x-1/2"
-              >
-                <QRCodeSVG value="https://github.com/katoyuta0219/DonationApp" />
-              </button>
-            </>
-          ): currentStep === 3 && (
-            <>
-              <section className="flex flex-col gap-6 w-82 mx-auto text-black">
-                <h2 className="Body16Medium">3. 発送完了</h2>
-                <p className="Body12Regular">発送が完了しました！</p>
-              </section>
-              <Button 
-                size="large"
-                text="ホームに戻る"
-                className="absolute bottom-10 left-1/2 -translate-x-1/2"
-                onClick={() => handleBackHome()}
-              />
-            </>
-          )}
-        </div>
+        <Button
+          size="large"
+          text={`${selectedIndex === null ? "選択してください" : shippingOptions[selectedIndex].text}`}
+          className="fixed bottom-16"
+          isDisabled={selectedIndex === null}
+          onClick={() => handleRouterPush()}
+        />
       </div>
     </main>
   );
